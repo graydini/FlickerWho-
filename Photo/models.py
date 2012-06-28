bd381ad784ac31466d3bcd73fc899dc0baf7327e
graydini <graydini@me.com>
2012-06-27 22:22:35 -0700

    Finished Flickr and Faceregonistion backend

--- Photo/models.py
+++ Photo/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Photo(models.Model):
 	p_id = models.IntegerField()
 	has_face = models.BooleanField()
-	name = models.CharField(max_length=50,blank=True,null=True)
-	face_x = models.IntegerField(blank=True,null=True)
-	face_y = models.IntegerField(blank=True,null=True)
-	face_width = models.IntegerField(blank=True,null=True)
+#	name = models.CharField(max_length=50,blank=True,null=True)
+#	face_x = models.IntegerField(blank=True,null=True)
+#	face_y = models.IntegerField(blank=True,null=True)
+#	face_width = models.IntegerField(blank=True,null=True)
 	pic = models.FilePathField(blank=True,null=True)
 	date_posted = models.IntegerField()
 class Tile(models.Model):
@@ -30,9 +31,32 @@ class Game(models.Model):
 	player1_guesses = models.IntegerField()
 	player2_guesses = models.IntegerField()
 	tiles = models.ManyToManyField('Tile')
-#def gather_images():
-#	import flickr_api as flickr
-
+def gatherImages():
+	import flickr_api as flickr
+	import Image
+	flickr.set_keys(settings.FLICKR_API,settings.FLICKR_SECRET)
+	try:
+		latest = Photo.objects.latest('id').date_posted 
+	except:
+		latest = 00000000
+	w = flickr.Walker(flickr.Photo.search, tags=settings.FLICKR_TAGS, min_upload_date =latest, sort="date-posted-asc")
+	for photo in w:
+		face_pic = recognizeFace(photo.getPhotoFile())
+		dbPhoto = Photo()
+		dbPhoto.p_id = photo.id
+		dbPhoto.date_posted = photo.dateuploaded
+		if not face_pic == False:
+			dbPhoto.has_face = True
+			pic_filename = settings.MEDIA_ROOT+'raw_photos/'+photo.id+'.jpg'
+			photo.save(pic_filename)
+			local_image = Image.open(pic_filename)
+			face_file = local_image.crop(face_pic)
+			face_file.save(settings.MEDIA_ROOT+'faces/'+photo.id+'.jpg','jpeg')
+			dbPhoto.pic = 'faces/'+photo.id+'.jpg'
+		else:
+			dbPhoto.has_face = False
+		dbPhoto.save()
+			
 def getFace(photo):
 	"""Takes a response from face api and returns the face_area of the first face found in a picture."""
 	nose = photo['tags'][0]['nose']['y']
@@ -44,11 +68,17 @@ def getFace(photo):
 	scale_h = height / 100.0
 	scale_w = width / 100.0
 	c_width = int((ratio * 9) * scale_w)
-	c_height = int((ratio * 9) * scale_h)
+	c_height = int((ratio * 9) * scale_w)
 	c_left = int((center * scale_w) - ((ratio * 6) * scale_w))
 	c_top = int((nose * scale_h) - ((ratio * 5) * scale_h))
 	face_area = (c_left,c_top,c_left+c_width,c_top+c_height)
 	return face_area
 
-
-
+def recognizeFace(photo_url):
+	from face_client import FaceClient
+	client = FaceClient(settings.FACES_API,settings.FACES_SECRET)
+	photo = client.faces_detect(photo_url)['photos'][0]
+	if len(photo['tags']) > 0:
+		return getFace(photo)
+	else:
+		return False
